39dbd1245da8f85bcc39e4e34cd93f660910b3f4
Eomaxl <souravsnigdh@gmail.com>
2019-08-29 12:13:50 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,20 +12,23 @@ import { HeaderComponent } from './Components/header/header.component';
 import { ChangeCaseDirective } from './directives/change-case.directive';
 import { DirectiveHostComponent } from './Components/directive-host/directive-host.component';
 import { UnlessDirective } from './directives/unless.directive';
+import { ParentComponent } from './Components/parent/parent.component';
 
 const appRouting:Routes = [
   { path:'home', component:HomeComponent },
   { path:'about', component:AboutComponent},
   { path:'blogview', component:BlogViewComponent},
   { path:'directive', component:DirectiveHostComponent},
+  { path:'parent', component:ParentComponent},
   { path:'',redirectTo:'/home',pathMatch:'full'},
-  { path:'**',redirectTo:'/home',pathMatch:'full'}
+  { path:'**',redirectTo:'/home',pathMatch:'full'},
+  
 ]
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRouting) ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, BlogViewComponent, HeaderComponent, ChangeCaseDirective, DirectiveHostComponent, UnlessDirective ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, BlogViewComponent, HeaderComponent, ChangeCaseDirective, DirectiveHostComponent, UnlessDirective, ParentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
