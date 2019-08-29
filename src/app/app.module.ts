ac299b5f36247b2126ce76e7d36418b515e49f33
Eomaxl <souravsnigdh@gmail.com>
2019-08-29 09:35:57 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -9,8 +10,15 @@ import { AboutComponent } from './Components/about/about.component';
 import { BlogViewComponent } from './Components/blog-view/blog-view.component';
 import { HeaderComponent } from './Components/header/header.component';
 
+const appRouting:Routes = [
+  { path:'home', component:HomeComponent },
+  { path:'about', component:AboutComponent},
+  { path:'blogview', component:BlogViewComponent}
+]
+
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRouting) ],
   declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, BlogViewComponent, HeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
